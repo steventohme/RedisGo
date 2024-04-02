6fe8878d3a59dc22efd94606b9d6185c38793107
Steven Tohme <steventohme59@gmail.com>
2024-04-01 20:37:55 -0400

    add reading resp support

--- main.go
+++ main.go
@@ -1,39 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "net"
-    "os"
+	"fmt"
+	"net"
 )
 
 func main() {
-    fmt.Println("Listening on port 6379")
-    l, err := net.Listen("tcp", ":6379")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	fmt.Println("Listening on port :6379")
 
-    conn, err := l.Accept()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	// Create a new server
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    defer conn.Close()
+	// Listen for connections
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    for {
-        buf := make([]byte, 1024)
-        _, err := conn.Read(buf)
-        if err != nil {
-            if err != io.EOF {
-                break
-            }
-            fmt.Println("Error reading from client: ", err.Error())
-            os.Exit(1)
-        }
-        conn.Write([]byte("+OK\r\n"))
-    }
-    
+	defer conn.Close()
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(value)
+
+		// ignore request and send back a PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
